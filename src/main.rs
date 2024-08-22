c6048ec6b5daa50d255c04b87fa4b9f2cb588203
Fadarrizz <aukegeerts@duck.com>
2024-08-22 10:40:35 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use std::io::{stdin, stdout, Write};
+
 use chess::Board;
+
+fn input(prompt: impl std::fmt::Display) -> String {
+    let mut s = String::new();
+
+    print!("{}", prompt);
+
+    let _ = stdout().flush();
+    
+    stdin()
+        .read_line(&mut s)
+        .expect("Did not enter a correct move");
+
+    return s;
+}
 
 fn main() {
     let board = Board::default();
 
     println!("{}", board);
+
+    let s = input(">>> ");
+    let s = s.trim().to_string();
+
+    dbg!(s);
 }
